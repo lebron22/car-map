1c894c8427180d08ea256cb73b78e5aa43ca1489
Kamil Bronisz <kbronisz8@gmail.com>
2020-01-15 14:21:47 +0100

    Updates

---
+++ precache-manifest.f120c30cfe1feb5b1d2336e864139084.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85b32630e0f7aa0fb9afab279564025",
+    "url": "/car-map/index.html"
+  },
+  {
+    "revision": "f90b80b0129266c671a8",
+    "url": "/car-map/static/css/2.7f124a9d.chunk.css"
+  },
+  {
+    "revision": "f310e6ccdc64b4a7bafe",
+    "url": "/car-map/static/css/main.6caa9531.chunk.css"
+  },
+  {
+    "revision": "f90b80b0129266c671a8",
+    "url": "/car-map/static/js/2.0080f3cb.chunk.js"
+  },
+  {
+    "revision": "e11a5003bc9c3b0e632cbc712eb7d7e5",
+    "url": "/car-map/static/js/2.0080f3cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f310e6ccdc64b4a7bafe",
+    "url": "/car-map/static/js/main.e36e5fae.chunk.js"
+  },
+  {
+    "revision": "64b4ef574cd45fbe2d30",
+    "url": "/car-map/static/js/runtime-main.17448afd.js"
+  },
+  {
+    "revision": "c0503a4186c6e66eecfa182b2824ac85",
+    "url": "/car-map/static/media/car-round_available-3.c0503a41.svg"
+  },
+  {
+    "revision": "b7314b99f9ed0aa696a7cce61ee531ee",
+    "url": "/car-map/static/media/car-round_unavailable-3.b7314b99.svg"
+  },
+  {
+    "revision": "e3c0ae30c91515bf8b751a928e85ab60",
+    "url": "/car-map/static/media/settings-white.e3c0ae30.svg"
+  }
+]);
